0f2079759500176b2199e7dd1eec9709f5b5f2ba
NoahHaile <134994605+NoahHaile@users.noreply.github.com>
2023-10-04 11:54:12 -0700

    Add files via upload

---
+++ lib/_Models/ProductM.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: file_names
+
+class ProductM {
+  int proID = 0;
+  int catagID = 0;
+  String proName = "";
+  double uPrice = 0;
+  int qty = 1;
+  String proDesc = "";
+  double avgRating = 0;
+  int numOfReviews = 0;
+  String imgUrl1 = "";
+  String imgUrl2 = "";
+  String imgUrl3 = "";
+}
